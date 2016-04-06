11fc8e34c5ce164c60508c48b1957e05e724b0f7
Yannik Behr <yannik@yanmail.de>
2016-04-06 16:17:40 +0200

    adding optional summary output

--- vssc3_pick_delays.py
+++ vssc3_pick_delays.py
@@ -69,9 +69,13 @@ class PickDelay:
                 pct, pt, tdiff, net, stat = _e
                 station = '%s.%s' % (net, stat)
                 if dbtype == 'postgresql':
-                    self.stations[station].append(tdiff.total_seconds())
+                    self.stations[station].append((tdiff.total_seconds(),
+                                                   pct.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+                                                   pt.strftime("%Y-%m-%dT%H:%M:%S.%f")))
                 elif dbtype == 'mysql':
-                    self.stations[station].append(float(tdiff))
+                    self.stations[station].append((float(tdiff),
+                                                   pct.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+                                                   pt.strftime("%Y-%m-%dT%H:%M:%S.%f")))
             fh = open(fout, 'w')
             json.dump(self.stations, fh)
             fh.close()
@@ -80,6 +84,38 @@ class PickDelay:
             self.stations = json.load(fh)
             fh.close()
 
+    def summary(self):
+        min_del = 1e38
+        max_del = -1e38
+        min_pct = UTCDateTime()
+        max_pct = UTCDateTime(0)
+        for _s in self.stations:
+            for _e in self.stations[_s]:
+                _d, _pct, _pt = _e
+                _pct = UTCDateTime(_pct)
+                _pt = UTCDateTime(_pt)
+                if _pct > max_pct:
+                    max_pct = _pct
+                    max_pct_st = _s
+                    max_pct_del = _d
+                if _pct < min_pct:
+                    min_pct = _pct
+                    min_pct_st = _s
+                    min_pct_del = _d
+                if _d < min_del:
+                    min_del = _d
+                    min_del_st = _s
+                    min_del_pct = _pct
+                if _d > max_del:
+                    max_del = _d
+                    max_del_st = _s
+                    max_del_pct = _pct
+
+        print "Earliest pick: %s %s (delay: %.2f)" % (min_pct_st, min_pct, min_pct_del)
+        print "Latest pick: %s %s (delay: %.2f)" % (max_pct_st, max_pct, max_pct_del)
+        print "Minimum delay: %s %s (delay: %.2f)" % (min_del_st, min_del_pct, min_del)
+        print "Maximum delay: %s %s (delay: %.2f)" % (max_del_st, max_del_pct, max_del)
+
     def plot_delays(self, fout, networks=[]):
         fig = plt.figure()
         ax = fig.add_subplot(111)
@@ -87,10 +123,12 @@ class PickDelay:
         for _s in self.stations.keys():
             net, stat = _s.split('.')
             if net in networks or '*' in networks:
-                delays[net] += self.stations[_s]
+                for _e in self.stations[_s]:
+                    _d, _pct, _pt = _e
+                    delays[net].append(_d)
         if len(delays.keys()) > 0:
             n, bins, patches = ax.hist([delays[_n] for _n in delays.keys()],
-                                           bins=np.arange(-30, 30, 0.5),
+                                           bins=np.arange(-30, 30, 1.0),
                                            histtype='barstacked',
                                            label=[_n for _n in delays.keys()],
                                            rwidth=1.0)
@@ -111,7 +149,8 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--database', help="Database name.")
     parser.add_argument('--dbtype', help="Database type ('postgresql' or 'mysql').")
     parser.add_argument('-o', '--jsonfile', help="Path for json file with pick delays.")
-    parser.add_argument('--plotfile', help="Path for png file showing pick delay distribution.")
+    parser.add_argument('--plotfile', help="Path for png file showing pick delay distribution.",
+                        default=None)
     parser.add_argument('--new', help="Reread data from database even if json file exists.",
                         action='store_true')
     parser.add_argument('--start', help="Give start time for the query e.g. 2014-12-31T12:11:05",
@@ -122,6 +161,8 @@ if __name__ == '__main__':
     network codes that you want to plot, e.g. 'CH,MN'")
     parser.add_argument('--pickmethod', help="Give the abbreviation that \
     describes your automatic picks. Defaults to 'Trigger'.", default='Trigger')
+    parser.add_argument('--summary', help="Print a summary of the results.",
+                        action='store_true')
     args = parser.parse_args()
     pd = PickDelay()
     pd.get_pick_delays(args.jsonfile, args.host, args.database, args.user,
@@ -134,4 +175,7 @@ if __name__ == '__main__':
             networks.append(_n)
     else:
         networks = ['*']
-    pd.plot_delays(args.plotfile, networks=networks)
+    if args.summary:
+        pd.summary()
+    if args.plotfile is not None:
+        pd.plot_delays(args.plotfile, networks=networks)
